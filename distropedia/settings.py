2fcc42b1dfcfa19cf52b52384a5513c83f7d7e4d
Alexander Lungu <lungualexbeny@gmail.com>
2024-02-22 15:52:12 +0000

    Generate django generic views

--- distropedia/settings.py
+++ distropedia/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['linuxdistropedia-d0c59f414e1b.herokuapp.com', 'localhost', '8000-alex025x-linuxdistroped-nygvn597bdh.ws-eu108.gitpod.io',  ]
 
