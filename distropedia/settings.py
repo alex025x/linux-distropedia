8e1ca2d5e03eb8273f4c254336526b85e7c2981a
Alexander Lungu <lungualexbeny@gmail.com>
2024-02-23 17:11:35 +0000

    Create base template of homepage

--- distropedia/settings.py
+++ distropedia/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashCloudinaryStorage'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'distropedia')]
