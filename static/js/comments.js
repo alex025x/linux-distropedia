1106e08149b26a684ff31bf4f0796996b862a1c8
Alexander Lungu <lungualexbeny@gmail.com>
2024-02-28 14:11:27 +0000

    Add image placeholder

--- static/js/comments.js
+++ static/js/comments.js
@@ -19,16 +19,7 @@ for (let button of editButtons) {
   });
 }
 
-/**
-* Initializes deletion functionality for the provided delete buttons.
-* 
-* For each button in the `deleteButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Updates the `deleteConfirm` link's href to point to the 
-* deletion endpoint for the specific comment.
-* - Displays a confirmation modal (`deleteModal`) to prompt 
-* the user for confirmation before deletion.
-*/
+
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("comment_id");
